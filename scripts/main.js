79696bdc9e4bcad34496c2cdf5aeb54a0f04c9fb
Superstarxalien <ernesto_vallenilla@hotmail.com>
2025-11-02 16:27:33 -0400

    add half of track presets, round exp to 8 decimals

--- scripts/main.js
+++ scripts/main.js
@@ -172,6 +172,361 @@ const gradingPoints =
 		laps: 3,
 		checkpoints: 2
 	},
+	34: // sub-zero peak
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	35: // launch base
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	36: // azure lake
+	{
+		laps: 5,
+		checkpoints: 0
+	},
+	37: // balloon park
+	{
+		laps: 5,
+		checkpoints: 0
+	},
+	38: // chrome gadget
+	{
+		laps: 5,
+		checkpoints: 0
+	},
+	39: // desert palace
+	{
+		laps: 5,
+		checkpoints: 1
+	},
+	40: // endless mine 1
+	{
+		laps: 5,
+		checkpoints: 1
+	},
+	41: // hard-boiled stadium
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	42: // hardhat havoc
+	{
+		laps: 5,
+		checkpoints: 1
+	},
+	43: // press garden
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	44: // pico park
+	{
+		laps: 12,
+		checkpoints: 0
+	},
+	45: // city escape
+	{
+		laps: 1,
+		checkpoints: 7
+	},
+	46: // palmtree panic
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	47: // darkvile castle
+	{
+		laps: 4,
+		checkpoints: 2
+	},
+	48: // scarlet gardens
+	{
+		laps: 6,
+		checkpoints: 0
+	},
+	49: // motobug motorway
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	50: // star light
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	51: // metropolis
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	52: // frozen production
+	{
+		laps: 2,
+		checkpoints: 3
+	},
+	53: // aqueduct crystal
+	{
+		laps: 3,
+		checkpoints: 1
+	},
+	54: // nova shore
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	55: // hydro city
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	56: // trap tower
+	{
+		laps: 5,
+		checkpoints: 2
+	},
+	57: // diamond dust
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	58: // blue mountain 1
+	{
+		laps: 3,
+		checkpoints: 1
+	},
+	59: // blue mountain 2
+	{
+		laps: 1,
+		checkpoints: 6
+	},
+	60: // speed highway
+	{
+		laps: 1,
+		checkpoints: 5
+	},
+	61: // carnival night
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	62: // virtual highway
+	{
+		laps: 3,
+		checkpoints: 1
+	},
+	63: // dark fortress
+	{
+		laps: 2,
+		checkpoints: 2
+	},
+	64: // spring yard
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	65: // labyrinth
+	{
+		laps: 2,
+		checkpoints: 3
+	},
+	66: // hot shelter
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	67: // sky sanctuary
+	{
+		laps: 2,
+		checkpoints: 3
+	},
+	68: // lost colony
+	{
+		laps: 2,
+		checkpoints: 4
+	},
+	69: // death egg
+	{
+		laps: 3,
+		checkpoints: 4
+	},
+	70: // 765 stadium
+	{
+		laps: 5,
+		checkpoints: 1
+	},
+	71: // skyscraper leaps
+	{
+		laps: 4,
+		checkpoints: 1
+	},
+	72: // green triangle
+	{
+		laps: 7,
+		checkpoints: 0
+	},
+	73: // zoned city
+	{
+		laps: 4,
+		checkpoints: 2
+	},
+	74: // sunset hill
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	75: // savannah citadel
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	76: // umbrella rushwinds
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	77: // avant garden
+	{
+		laps: 3,
+		checkpoints: 3
+	},
+	78: // bigtime breakdown
+	{
+		laps: 4,
+		checkpoints: 2
+	},
+	79: // vantablack violet
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	80: // chaos chute
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	81: // dimension disaster
+	{
+		laps: 5,
+		checkpoints: 1
+	},
+	82: // aurora atoll
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	83: // daytona speedway
+	{
+		laps: 4,
+		checkpoints: 1
+	},
+	84: // turquoise hill
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	85: // weiss waterway
+	{
+		laps: 5,
+		checkpoints: 0
+	},
+	86: // ice paradise
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	87: // sunsplashed getaway
+	{
+		laps: 6,
+		checkpoints: 0
+	},
+	88: // fae falls
+	{
+		laps: 2,
+		checkpoints: 2
+	},
+	89: // azure axiom
+	{
+		laps: 1,
+		checkpoints: 7
+	},
+	90: // hanagumi hall
+	{
+		laps: 5,
+		checkpoints: 1
+	},
+	91: // aerial highlands
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	92: // crispy canyon
+	{
+		laps: 4,
+		checkpoints: 1
+	},
+	93: // technology tundra
+	{
+		laps: 2,
+		checkpoints: 3
+	},
+	94: // operator's overspace
+	{
+		laps: 3,
+		checkpoints: 3
+	},
+	95: // mega green hill
+	{
+		laps: 7,
+		checkpoints: 1
+	},
+	96: // mega bridge
+	{
+		laps: 6,
+		checkpoints: 1
+	},
+	97: // mega lava reef
+	{
+		laps: 5,
+		checkpoints: 0
+	},
+	98: // mega ice cap
+	{
+		laps: 5,
+		checkpoints: 1
+	},
+	99: // mega scrap brain
+	{
+		laps: 5,
+		checkpoints: 1
+	},
+	100: // wavecrash dimension
+	{
+		laps: 5,
+		checkpoints: 1
+	},
+	101: // nightfall dimension
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	102: // voiddance dimension
+	{
+		laps: 3,
+		checkpoints: 2
+	},
+	103: // cloudtop dimension
+	{
+		laps: 2,
+		checkpoints: 3
+	},
+	104: // gravtech dimension
+	{
+		laps: 3,
+		checkpoints: 2
+	},
 }
 
 function calcExp(pos, isFinalLap) {
@@ -209,9 +564,9 @@ function calcExp(pos, isFinalLap) {
 	const mult = numGradingPointsBonus - (numGradingPoints - 1);
 
 	if (isFinalLap)
-		return ((((1 - currGradingFactor) * targetMinExp) + (currGradingFactor * targetMaxExp)) * mult);
+		return Math.round(((((1 - currGradingFactor) * targetMinExp) + (currGradingFactor * targetMaxExp)) * mult) * 100000000) / 100000000;
 	else
-		return (((1 - currGradingFactor) * targetMinExp) + (currGradingFactor * targetMaxExp));
+		return Math.round((((1 - currGradingFactor) * targetMinExp) + (currGradingFactor * targetMaxExp)) * 100000000) / 100000000;
 }
 
 trackpresets.onclick = () => {
